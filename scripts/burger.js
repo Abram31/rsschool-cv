52e2df4d712dce99ce6a3adc31185e018f326326
Abram31 <a447623993@gmail.com>
2022-02-23 12:20:18 +0300

    feat: add animation

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,15 +1,13 @@
-const burger  = document.querySelector('.header_burger');
-const menu = document.querySelectorAll('.header_menu');
-console.log(burger);
-console.log(menu);
+var burger = document.querySelector('.header_burger');
+var menu = document.querySelectorAll('.header_menu');
+var firstLine = burger.querySelector('.burger-first-line');
+var secondLine = burger.querySelector('.burger-second-line');
+var thirdLine = burger.querySelector('.burger-third-line');
 burger.addEventListener('click', function () {
-    menu.forEach(block => {
+    menu.forEach(function (block) {
         block.classList.toggle('active');
-    })   
-}  
-    
-
-)
-
-
-
+    });
+    firstLine.classList.toggle('burger-first-line-active');
+    secondLine.classList.toggle('burger-second-line-active');
+    thirdLine.classList.toggle('burger-third-line-active');
+});
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,15 +1,26 @@
-const hrefs = document.querySelectorAll('a[href*="#"]');
-console.log(hrefs);
-for (let href of hrefs ) {
-    href.addEventListener('click',function (event) {
-    event.preventDefault();
-    const blockID = href.getAttribute('href');
+// ----------------------Smooth scroll ----------------------------------------
+var hrefs = document.querySelectorAll('a[href*="#"]');
+var _loop_1 = function (href) {
+    href.addEventListener('click', function (event) {
+        event.preventDefault();
+        var blockID = href.getAttribute('href');
         document.querySelector('' + blockID).scrollIntoView({
-        behavior:"smooth",
-        block:"center"
-    })
-    
-} )
+            behavior: "smooth",
+            block: "center"
+        });
+    });
+};
+for (var _i = 0, hrefs_1 = hrefs; _i < hrefs_1.length; _i++) {
+    var href = hrefs_1[_i];
+    _loop_1(href);
 }
-
-
+// --------------------------Change Background Header with Scroll-----------------------------------
+var header = document.querySelector('.header');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 135) {
+        header.classList.add('header-background-color-scroll');
+    }
+    else {
+        header.classList.remove('header-background-color-scroll');
+    }
+});
